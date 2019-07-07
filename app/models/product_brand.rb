198dabf36a0a07c125d94bdedc9e7c3546f489c6
gnguyen <gnguyen5464@gmail.com>
2019-07-06 21:04:22 -0400

    Add validation to Store and ProductBrand models

--- app/models/product_brand.rb
+++ app/models/product_brand.rb
@@ -1,3 +1,5 @@
 class ProductBrand < ApplicationRecord
   belongs_to :store
+
+  validates_presence_of :brand_name
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,5 @@
 class Store < ApplicationRecord
   has_many :product_brands
+
+  validates_presence_of :store_name, :address, :city, :state, :zipcode
 end
