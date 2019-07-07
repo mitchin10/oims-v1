ccc82acf4df8f80f6d5814a8bccb4a697a6d7d35
gnguyen <gnguyen5464@gmail.com>
2019-07-06 21:16:06 -0400

    Add column name to Admin_User.

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_220306) do
   end
 
   create_table "admin_users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
