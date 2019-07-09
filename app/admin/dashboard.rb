f879251066d8f90f2506dd7d7d2ef3fe307bed8a
gnguyen <gnguyen5464@gmail.com>
2019-07-09 16:46:31 -0400

    Add link to all Product Items

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -35,34 +35,9 @@ ActiveAdmin.register_page "Dashboard" do
               link_to bn.product_brand.brand_name, admin_product_brand_path(bn.product_brand_id)
             end
           end
+          strong { link_to "All Products", [:admin, :product_items] }
         end
       end # column
     end # columns
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
